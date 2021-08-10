1682f77816bbc4b72950182c606396756b306889
Jacob Sullivan <jay@jaysull.dev>
2021-08-09 22:06:25 -0500

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -28,13 +28,13 @@ pub struct HttpMessage {
 	resource: Option<String>,
 	status_code: Option<[u8; 3]>,
 	status_message: Option<String>,
-	headers: HashMap<String, String>
+	headers: HashMap<String, String>,
 }
 
 fn increment_position(bytes: &Vec<u8>, position: &mut usize, increment_by: usize) -> Result<(), Box<dyn Error>> {
 	// Check if new position is past the end of the json string
 	if *position + increment_by > bytes.len() {
-			return Err("Unexpectedly reached end of request".into());
+		return Err("Unexpectedly reached end of request".into());
 	}
 
 	*position += increment_by;
@@ -42,152 +42,111 @@ fn increment_position(bytes: &Vec<u8>, position: &mut usize, increment_by: usize
 	Ok(())
 }
 
-fn get_byte_at_offset(bytes: &Vec<u8>, position: &usize, offset: usize) -> Result<u8, Box<dyn Error>> {
-	// Check if char is past the end of the json string
-	if *position + offset > bytes.len() - 1 {
-		return Err("Unexpectedly reached end of request".into());
+fn get_byte(bytes: &Vec<u8>, position: &mut usize) -> Result<u8, Box<dyn Error>> {
+	let b = bytes[*position];
+	increment_position(bytes, position, 1)?;
+
+	return Ok(b);
+}
+
+fn check_bytes(bytes: &Vec<u8>, position: &mut usize, check: Vec<u8>) -> Result<bool, Box<dyn Error>> {
+	// Check that each byte matches
+	for check_byte in check {
+		if get_byte(bytes, position)? != check_byte {
+			return Ok(false);
+		}
 	}
 
-	Ok(bytes[*position + offset])
+	Ok(true)
+}
+
+fn is_white_space(b: u8) -> bool {
+	b == 32u8
+}
+
+fn skip_white_space(bytes: &Vec<u8>, position: &mut usize) -> Result<(), Box<dyn Error>> {
+	while is_white_space(bytes[*position]) {
+		increment_position(bytes, position, 1)?;
+	}
+
+	Ok(())
 }
 
 fn determine_request(bytes: &Vec<u8>) -> Result<bool, Box<dyn Error>> {
+	// H, T is response
+	// H, E | G | P | D | O is request
 	match bytes[0] {
-		72u8 => {
-			match bytes[1] {
-				84u8 => Ok(false),
-				69u8 => Ok(true),
-				_ => Err("Malformed HTTP Message".into())
-			}
+		72u8 => match bytes[1] {
+			84u8 => Ok(false),
+			69u8 => Ok(true),
+			_ => Err("Malformed HTTP Message".into()),
 		},
 		71u8 | 80u8 | 68u8 | 79u8 => Ok(true),
-		_ => Err("Malformed HTTP Message".into())
+		_ => Err("Malformed HTTP Message".into()),
 	}
 }
 
 fn get_method(bytes: &Vec<u8>, position: &mut usize) -> Result<HttpMethod, Box<dyn Error>> {
-	match get_byte_at_offset(bytes, position, 0)? {
+	match get_byte(bytes, position)? {
 		// G
 		71u8 => {
 			// E, T, space
-			if
-				get_byte_at_offset(bytes, position, 1)? == 69u8 &&
-				get_byte_at_offset(bytes, position, 2)? == 84u8 &&
-				get_byte_at_offset(bytes, position, 3)? == 32u8
-			{
-				increment_position(bytes, position, 4)?;
-
-				Ok(HttpMethod::GET)
-			} else {
-				Err("Malformed HTTP Message".into())
+			if check_bytes(bytes, position, vec![69u8, 84u8, 32u8])? {
+				return Ok(HttpMethod::GET);
 			}
-		},
+		}
 		// P
 		80u8 => {
-			match get_byte_at_offset(bytes, position, 1)? {
+			match get_byte(bytes, position)? {
 				// U
 				85u8 => {
 					// T, space
-					if
-						get_byte_at_offset(bytes, position, 2)?  == 84u8 &&
-						get_byte_at_offset(bytes, position, 3)?  == 32u8
-					{
-						increment_position(bytes, position, 4)?;
-		
-						Ok(HttpMethod::PUT)
-					} else {
-						Err("Malformed HTTP Message".into())
+					if check_bytes(bytes, position, vec![84u8, 32u8])? {
+						return Ok(HttpMethod::PUT);
 					}
-				},
+				}
 				// O
 				79u8 => {
 					// S, T, space
-					if
-						get_byte_at_offset(bytes, position, 2)?  == 83u8 &&
-						get_byte_at_offset(bytes, position, 3)?  == 84u8 &&
-						get_byte_at_offset(bytes, position, 4)?  == 32u8
-					{
-						increment_position(bytes, position, 5)?;
-		
-						Ok(HttpMethod::POST)
-					} else {
-						Err("Malformed HTTP Message".into())
+					if check_bytes(bytes, position, vec![83u8, 34u8, 32u8])? {
+						return Ok(HttpMethod::POST);
 					}
-				},
+				}
 				// A
 				65u8 => {
 					// T, C, H, space
-					if
-						get_byte_at_offset(bytes, position, 2)?  == 84u8 &&
-						get_byte_at_offset(bytes, position, 3)?  == 67u8 &&
-						get_byte_at_offset(bytes, position, 4)?  == 72u8 &&
-						get_byte_at_offset(bytes, position, 5)?  == 32u8
-					{
-						increment_position(bytes, position, 6)?;
-		
-						Ok(HttpMethod::PATCH)
-					} else {
-						Err("Malformed HTTP Message".into())
+					if check_bytes(bytes, position, vec![84u8, 67u8, 72u8, 32u8])? {
+						return Ok(HttpMethod::PATCH);
 					}
-				},
-				_ => Err("Malformed HTTP Message".into())
+				}
+				_ => {}
 			}
-		},
+		}
 		// H
 		72u8 => {
 			// E, A, D, space
-			if
-				get_byte_at_offset(bytes, position, 2)?  == 69u8 &&
-				get_byte_at_offset(bytes, position, 3)?  == 65u8 &&
-				get_byte_at_offset(bytes, position, 4)?  == 68u8 &&
-				get_byte_at_offset(bytes, position, 5)?  == 32u8 
-			{
-				increment_position(bytes, position, 6)?;
-
-				Ok(HttpMethod::HEAD)
-			} else {
-				Err("Malformed HTTP Message".into())
+			if check_bytes(bytes, position, vec![69u8, 65u8, 68u8, 32u8])? {
+				return Ok(HttpMethod::HEAD);
 			}
-		},
+		}
 		// D
 		68u8 => {
 			// E, L, E, T, E, space
-			if
-				get_byte_at_offset(bytes, position, 2)?  == 69u8 &&
-				get_byte_at_offset(bytes, position, 3)?  == 76u8 &&
-				get_byte_at_offset(bytes, position, 4)?  == 69u8 &&
-				get_byte_at_offset(bytes, position, 5)?  == 84u8 &&
-				get_byte_at_offset(bytes, position, 6)?  == 69u8 &&
-				get_byte_at_offset(bytes, position, 7)?  == 32u8 
-			{
-				increment_position(bytes, position, 8)?;
-
-				Ok(HttpMethod::DELETE)
-			} else {
-				Err("Malformed HTTP Message".into())
+			if check_bytes(bytes, position, vec![69u8, 76u8, 69u8, 84u8, 69u8, 32u8])? {
+				return Ok(HttpMethod::DELETE);
 			}
-		},
+		}
 		// O
 		79u8 => {
 			// P, T, I, O, N, S, space
-			if
-				get_byte_at_offset(bytes, position, 2)?  == 80u8 &&
-				get_byte_at_offset(bytes, position, 3)?  == 84u8 &&
-				get_byte_at_offset(bytes, position, 4)?  == 73u8 &&
-				get_byte_at_offset(bytes, position, 5)?  == 79u8 &&
-				get_byte_at_offset(bytes, position, 6)?  == 78u8 &&
-				get_byte_at_offset(bytes, position, 7)?  == 83u8 &&
-				get_byte_at_offset(bytes, position, 8)?  == 32u8
-			{
-				increment_position(bytes, position, 9)?;
-
-				Ok(HttpMethod::OPTIONS)
-			} else {
-				Err("Malformed HTTP Message".into())
+			if check_bytes(bytes, position, vec![80u8, 84u8, 73u8, 79u8, 78u8, 32u8, 32u8])? {
+				return Ok(HttpMethod::OPTIONS);
 			}
-		},
-		_ => Err("Malformed HTTP Message".into())
+		}
+		_ => {}
 	}
+
+	Err("Malformed HTTP Message".into())
 }
 
 fn get_resource(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<dyn Error>> {
@@ -197,9 +156,8 @@ fn get_resource(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<dyn
 
 	let mut resource = String::new();
 
-	while bytes[*position] != 32u8 {
+	while get_byte(bytes, position)? != 32u8 {
 		resource.push(bytes[*position] as char);
-		increment_position(bytes, position, 1)?;
 	}
 
 	increment_position(bytes, position, 1)?;
@@ -208,74 +166,40 @@ fn get_resource(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<dyn
 }
 
 fn get_version(bytes: &Vec<u8>, position: &mut usize) -> Result<HttpVersion, Box<dyn Error>> {
-	let http_check: [u8; 5] = [72u8, 84u8, 84u8, 80u8, 47u8];
+	// H, T, T, P, /
+	check_bytes(bytes, position, vec![72u8, 84u8, 84u8, 80u8, 47u8])?;
 
-	for byte in http_check {
-		if bytes[*position] != byte {
-			return Err("Malformed HTTP Message".into());
-		}
-
-		increment_position(bytes, position, 1)?;
-	}
-
-	match bytes[*position] {
+	match get_byte(bytes, position)? {
 		// 0
 		48u8 => {
 			// ., 9
-			if
-				get_byte_at_offset(bytes, position, 1)? == 46u8 &&
-				get_byte_at_offset(bytes, position, 2)? == 57u8
-			{
-				increment_position(bytes, position, 3)?;
+			if check_bytes(bytes, position, vec![46u8, 47u8])? {
 				return Ok(HttpVersion::Version0_9);
 			}
-
-			return Err("Malformed HTTP Message".into());
-		},
+		}
 		// 1
 		49u8 => {
 			// .
-			if get_byte_at_offset(bytes, position, 1)? != 46u8 {
-				return Err("Malformed HTTP Message".into());
-			}
+			check_bytes(bytes, position, vec![46u8])?;
 
-			match get_byte_at_offset(bytes, position, 2)? {
+			match get_byte(bytes, position)? {
 				// 0
-				48u8 => {
-					increment_position(bytes, position, 3)?;
-					return Ok(HttpVersion::Version1_0);
-				},
+				48u8 => return Ok(HttpVersion::Version1_0),
 				// 1
-				49u8 => {
-					increment_position(bytes, position, 3)?;
-					return Ok(HttpVersion::Version1_1);
-				},
-				_ => return Err("Malformed HTTP Message".into())
-			}
-		},
+				49u8 => return Ok(HttpVersion::Version1_1),
+				_ => ()
+			};
+		}
 		// 2
-		50u8 => {
-			// ., 0
-			if
-				get_byte_at_offset(bytes, position, 1)? == 46u8 &&
-				get_byte_at_offset(bytes, position, 2)? == 48u8
-			{
-				increment_position(bytes, position, 3)?;
-				return Ok(HttpVersion::Version2_0);
-			} else {
-				return Err("Malformed HTTP Message".into());
-			}
-		},
-		_ => return Err("Malformed HTTP Message".into())
+		50u8 => return Ok(HttpVersion::Version2_0),
+		_ => ()
 	}
+
+	return Err("Malformed HTTP Message".into());
 }
 
 fn check_and_go_past_end_line(bytes: &Vec<u8>, position: &mut usize) -> Result<(), Box<dyn Error>> {
-	if
-		bytes[*position] == 13u8 &&
-		get_byte_at_offset(bytes, position, 1)? == 10u8
-	{
-		increment_position(bytes, position, 2)?;
+	if check_bytes(bytes, position, vec![13u8, 10u8])? {
 		return Ok(());
 	}
 
@@ -285,16 +209,11 @@ fn check_and_go_past_end_line(bytes: &Vec<u8>, position: &mut usize) -> Result<(
 fn get_header_key(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<dyn Error>> {
 	let mut header_key: String = String::new();
 
-	while bytes[*position] != 58u8 {
+	while get_byte(bytes, position)? != 58u8 {
 		header_key.push(bytes[*position] as char);
-		increment_position(bytes, position, 1)?;
 	}
 
 	increment_position(bytes, position, 1)?;
-
-	if bytes[*position] == 32u8 {
-		increment_position(bytes, position, 1)?;
-	}
 
 	Ok(header_key)
 }
@@ -302,9 +221,10 @@ fn get_header_key(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<d
 fn get_header_value(bytes: &Vec<u8>, position: &mut usize) -> Result<String, Box<dyn Error>> {
 	let mut header_value: String = String::new();
 
-	while bytes[*position] != 13u8 {
+	skip_white_space(bytes, position)?;
+
+	while get_byte(bytes, position)? != 13u8 {
 		header_value.push(bytes[*position] as char);
-		increment_position(bytes, position, 1)?;
 	}
 
 	Ok(header_value)
@@ -331,8 +251,8 @@ fn determine_headers(bytes: &Vec<u8>, position: &mut usize) -> Result<HashMap<St
 
 impl HttpMessage {
 	fn new(bytes: &Vec<u8>) -> Result<HttpMessage, Box<dyn Error>> {
-		if (bytes.len() == 0) {
-			return Err("Empty request".into());
+		if bytes.len() < 2 {
+			return Err("Malformed HTTP Message".into());
 		}
 
 		let mut position: usize = 0;
@@ -340,8 +260,8 @@ impl HttpMessage {
 		let mut method: Option<HttpMethod> = None;
 		let mut resource: Option<String> = None;
 		let version: HttpVersion;
-		let mut status_code: Option<[u8; 3]> = None;
-		let mut status_message: Option<String> = None;
+		let status_code: Option<[u8; 3]> = None;
+		let status_message: Option<String> = None;
 
 		// Start Line
 		if is_request {
@@ -365,7 +285,7 @@ impl HttpMessage {
 			resource,
 			status_code,
 			status_message,
-			headers
+			headers,
 		};
 
 		Ok(http_message)
